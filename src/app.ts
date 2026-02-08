6b6e266711e5ea283a64db2d49e8b2c70d31d7de
Nikunj Vadher <nikunjjv27@gail.com>
2026-02-08 12:50:13 +0530

    use error handling and cofig for env

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
-import express from "express";
+import express, { type Request, type Response, type NextFunction } from "express";
 import config from "./config/config.js";
+import type { HttpError } from "http-errors";
+import createHttpError from "http-errors";
 
 const app = express();
 const port = config.PORT;
 
+app.get('/', (req: Request, res: Response) => {
+    const error = createHttpError(404, 'Something went wrong');
+    throw error;
+});
 
-app.get("/", (req, res , next) => {
-    res.send("Hello World!");
-    next();
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode;
+
+    return res.json(statusCode).json({
+        message: err.message,
+        stack: config.env === "development" ? err.stack : '',
+    })
 });
 
 app.listen(port, () => {
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ Conf();
 
 const config = {
     PORT: process.env.PORT,
-    DB_URL: process.env.MONGODB_CONNECTION_URL
+    DB_URL: process.env.MONGODB_CONNECTION_URL,
+    env: process.env.NODE_ENV
 }
 
 export default config;
