8f5ef701dcb5217b652ebe77c00c0a6f14bce8df
Nikunj Vadher <nikunjjv27@gail.com>
2026-02-08 13:52:22 +0530

    add user validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,14 +1,28 @@
 import { type Request, type NextFunction, type Response } from "express"
 import createHttpError from "http-errors";
+import userModal from "./userModal";
 
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
-    console.log(req.body);
+
+
     if (!name || !email || !password) {
         const error = createHttpError(400, "All fields are required");
         return next(error);
     }
+
+
+    //db call
+    const user = await userModal.findOne({ email });
+    if (user) {
+        const error = createHttpError(400, "User already exists");
+        return next(error);
+    }
+
+
+
+
     res.json({ message: "User Registered" })
 }
 
