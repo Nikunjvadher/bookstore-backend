77985e60319713854a18fce97892d4806d2326a1
Nikunj Vadher <nikunjjv27@gail.com>
2026-02-09 21:20:26 +0530

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,12 @@
 import { type NextFunction, type Request, type Response } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
+import bookModal from "./bookModal";
+import fs from "node:fs";
+
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-    // const { title, author, genre, description, coverImage, bookPdf } = req.body;
+    const { title, author, genre, description, coverImage, bookPdf } = req.body;
 
     // console.log(req.files);
 
@@ -19,34 +22,51 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     const filename = coverImageFile.filename;
     const filePath = path.resolve(__dirname, '../../public/data/uploads', filename);
 
-    const uploadResults = await cloudinary.uploader.upload(filePath, {
-        filename_override: filename,
-        folder: 'book-cover',
-        format: coverImageMimeType,
-    });
-
-    const fileDetails = files.bookPdf?.[0];
-    console.log("Book location",fileDetails?.originalname);
-
-    if (!fileDetails) {
-        return next(new Error("Book PDF is required"));
-    }
-    const bookName = fileDetails.filename;
-    const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookName);
-
     try {
+        const uploadResults = await cloudinary.uploader.upload(filePath, {
+            filename_override: filename,
+            folder: 'book-cover',
+            format: coverImageMimeType,
+        });
+
+        const fileDetails = files.bookPdf?.[0];
+        if (!fileDetails) {
+            return next(new Error("Book PDF is required"));
+        }
+
+        const bookName = fileDetails.filename;
+        const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookName);
         const bookUploadResults = await cloudinary.uploader.upload(bookFilePath, {
             resource_type: 'raw',
             filename_override: bookName,
             folder: 'book-pdf',
         });
-        console.log("uploaded cover image:", bookUploadResults);
+
+        const newBook = await bookModal.create({
+            title: req.body.title,
+            author: "69884da0c24fe473e958391b" as any,
+            // author: req.body.author,
+            genre: req.body.genre,
+            description: req.body.description,
+            coverImage: uploadResults.secure_url,
+            bookPdf: bookUploadResults.secure_url,
+        })
+
+        await fs.promises.unlink(filePath);
+        await fs.promises.unlink(bookFilePath);
+
+        res.status(201).json(newBook);
+         
+        // console.log("uploaded results:", uploadResults);
+        // console.log("uploaded cover image:", bookUploadResults);
     } catch (error) {
         return next(new Error("Error uploading book PDF"))
     }
 
+    // delete temp files
 
-    console.log("uploaded results:", uploadResults);
+
+
 
     res.json({ message: "Book Created" });
 };
