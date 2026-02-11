1ac53c9fd4f50924b31ee1534f5a35e7cb671331
Nikunj Vadher <nikunjjv27@gmail.com>
2026-02-11 21:00:24 +0530

    update updateBook endpoint to only change updated fields

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -66,66 +66,62 @@ const updateBook = async (req: Request, res: Response, next: NextFunction) => {
     const { title, description, genre } = req.body;
     const bookId = req.params.bookid;
 
-    const book = await bookModal.findById(bookId);
-    if (!book) {
-        return next(new Error("Book not found"));
-    }
-
-    const _req = req as AuthRequest;
-    if (book.author.toString() !== _req.userId) {
-        return next(new Error("You are not authorized to update this book"));
-    }
-
-    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-
-    let completeCoverImage = '';
-    const coverImageFile = files.coverImage?.[0];
-    if (coverImageFile) {
-        const bookName = coverImageFile.filename;
-        const coverImageMimeType = coverImageFile.mimetype.split('/').pop() as string;
-        const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookName);
-
-        const bookUploadResults = await cloudinary.uploader.upload(bookFilePath, {
-            filename_override: bookName,
-            folder: 'book-cover',
-            format: coverImageMimeType,
-        });
-        completeCoverImage = bookUploadResults.secure_url;
-        await fs.promises.unlink(bookFilePath);
-    }
-
-    let completeFileName = '';
-    const bookPdfFile = files.bookPdf?.[0];
-    if (bookPdfFile) {
-        const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookPdfFile.filename);
-        const bookFileName = bookPdfFile.filename;
-
-        const bookUploadResults = await cloudinary.uploader.upload(bookFilePath, {
-            resource_type: 'raw',
-            filename_override: bookFileName,
-            folder: 'book-pdf',
-        });
-
-        completeFileName = bookUploadResults.secure_url;
-        await fs.promises.unlink(bookFilePath);
-    }
-
-    const updateBook = await bookModal.findByIdAndUpdate(
-        {
-            _id: bookId
-        },
-        {
-            title,
-            description,
-            genre,
-            coverImage: completeCoverImage,
-            bookPdf: completeFileName
-        },
-        {
-            new: true
+    try {
+        const book = await bookModal.findById(bookId);
+        if (!book) {
+            return next(new Error("Book not found"));
         }
-    )
 
-    res.json(updateBook);
+        const _req = req as AuthRequest;
+        if (book.author.toString() !== _req.userId) {
+            return next(new Error("You are not authorized to update this book"));
+        }
+
+        const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+        const updateFields: any = {};
+
+        if (title) updateFields.title = title;
+        if (description) updateFields.description = description;
+        if (genre) updateFields.genre = genre;
+
+        const coverImageFile = files.coverImage?.[0];
+        if (coverImageFile) {
+            const bookName = coverImageFile.filename;
+            const coverImageMimeType = coverImageFile.mimetype.split('/').pop() as string;
+            const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookName);
+
+            const bookUploadResults = await cloudinary.uploader.upload(bookFilePath, {
+                filename_override: bookName,
+                folder: 'book-cover',
+                format: coverImageMimeType,
+            });
+            updateFields.coverImage = bookUploadResults.secure_url;
+            await fs.promises.unlink(bookFilePath);
+        }
+
+        const bookPdfFile = files.bookPdf?.[0];
+        if (bookPdfFile) {
+            const bookFilePath = path.resolve(__dirname, '../../public/data/uploads', bookPdfFile.filename);
+            const bookFileName = bookPdfFile.filename;
+
+            const bookUploadResults = await cloudinary.uploader.upload(bookFilePath, {
+                resource_type: 'raw',
+                filename_override: bookFileName,
+                folder: 'book-pdf',
+            });
+            updateFields.bookPdf = bookUploadResults.secure_url;
+            await fs.promises.unlink(bookFilePath);
+        }
+
+        const updatedBook = await bookModal.findByIdAndUpdate(
+            bookId,
+            updateFields,
+            { new: true }
+        );
+
+        res.json(updatedBook);
+    } catch (error) {
+        return next(new Error("Error updating book"));
+    }
 }
 export { createBook, updateBook };
